82fcd2c60df102868aeb697410aa2089369e000b
“gauravmandal27” <gauravmandal02@gmail.com>
2024-04-22 10:44:34 +0530

    "Added the implementation"

--- idea-java-workspace/static-fields-and-methods/src/accountmanagement/Account.java
+++ idea-java-workspace/static-fields-and-methods/src/accountmanagement/Account.java
@@ -5,18 +5,41 @@ public class Account {
     private long accountNo;
     private String customerName;
     private double balance;
+    private static int totalAccount;
 
+
+    public Account(long accountNo, String customerName, double balance) {
+        this.accountNo = accountNo;
+        this.customerName = customerName;
+        this.balance = balance;
+        totalAccount+=1;
+    }
 
     public void deposit(double amount){
+        this.balance+=amount;
+        System.out.println("Deposit of "+amount+" complete, Updated Balance is:"+balance);
 
     }
 
     public void withdraw(double amount){
 
+        if((this.balance-=amount)>0){
+            this.balance-=amount;
+            System.out.println("Withdrawal of "+amount+" complete, Updated Balance is:"+balance);
+        }
+        else{
+            System.out.println("Low Balance!!");
+        }
     }
-
-    public void showAccountInfo(){
-
+    public static String totalAccountsCreated(){
+        return "Total number of accounts is "+totalAccount;
+    }
+    public String showAccountInfo(){
+        return "Account{" +
+                "accountNo=" + accountNo +
+                ", customerName='" + customerName + '\'' +
+                ", balance=" + balance +
+                '}';
     }
 
 }
--- idea-java-workspace/static-fields-and-methods/src/accountmanagement/AccountManagement.java
+++ idea-java-workspace/static-fields-and-methods/src/accountmanagement/AccountManagement.java
@@ -5,10 +5,19 @@ public class AccountManagement {
     public static void main(String[] args) {
 
         // Create few accounts
+        Account a1=new Account(2345,"Ram",2600);
+        Account a2=new Account(3654,"Shyam ",13600);
+        Account a3=new Account(2638,"Ram",1400);
+        Account a4=new Account(4892,"Ram",500);
+
 
         // Display the no. of accounts created
-
+        System.out.println(Account.totalAccountsCreated());
         // Do the basic operations(deposit/withdraw)
+        a1.withdraw(200);
+        a3.deposit(400);
+        a2.withdraw(500);
+        a4.withdraw(200);
 
 
     }
